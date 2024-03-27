fe011e04fea2d538df62f2057d9b17823c30690f
s0t-chit-0n0nd <lakshay21061@iiitd.ac.in>
2024-03-27 17:29:53 +0530

    Change in assignment- added ratio code

--- Assignment_3/EncDec/changerollno.py
+++ Assignment_3/EncDec/changerollno.py
@@ -10,7 +10,7 @@ class AlteredMNIST:
     Write code to load Dataset
     """
     pass
-    
+
 class Encoder:
     """
     Write code for Encoder ( Logits/embeddings shape must be [batch_size,channel,height,width] )
@@ -35,9 +35,9 @@ class VAELossFn:
     """
     pass
 
-def ParameterSelector():
+def ParameterSelector(E, D):
     """
-    Write code for selecting parameters to train 
+    Write code for selecting parameters to train
     """
     pass
 
@@ -47,7 +47,7 @@ class AETrainer:
     Write code for training AutoEncoder here.
     
     for each 10th minibatch use only this print statement
-    print(">>>>> Epoch:{}, Minibatch:{}, Loss:{}, Similarity:{}".format(epoch,minibatch,loss,accuracy))
+    print(">>>>> Epoch:{}, Minibatch:{}, Loss:{}, Similarity:{}".format(epoch,minibatch,loss,similarity))
     
     for each epoch use only this print statement
     print("----- Epoch:{}, Loss:{}, Similarity:{}")
@@ -62,7 +62,7 @@ class VAETrainer:
     Write code for training Variational AutoEncoder here.
     
     for each 10th minibatch use only this print statement
-    print(">>>>> Epoch:{}, Minibatch:{}, Loss:{}, Similarity:{}".format(epoch,minibatch,loss,accuracy))
+    print(">>>>> Epoch:{}, Minibatch:{}, Loss:{}, Similarity:{}".format(epoch,minibatch,loss,similarity))
     
     for each epoch use only this print statement
     print("----- Epoch:{}, Loss:{}, Similarity:{}")
@@ -105,7 +105,7 @@ class CVAE_Trainer:
     Write code for training Conditional Variational AutoEncoder here.
     
     for each 10th minibatch use only this print statement
-    print(">>>>> Epoch:{}, Minibatch:{}, Loss:{}, Similarity:{}".format(epoch,minibatch,loss,accuracy))
+    print(">>>>> Epoch:{}, Minibatch:{}, Loss:{}, Similarity:{}".format(epoch,minibatch,loss,similarity))
     
     for each epoch use only this print statement
     print("----- Epoch:{}, Loss:{}, Similarity:{}")
@@ -123,13 +123,33 @@ class CVAE_Generator:
     def save_image(digit, save_path):
         pass
 
-def peak_signal_to_noise_ratio(sample, original):
-    """
-    Write code to calculate PSNR. Return in float
-    """
+def peak_signal_to_noise_ratio(img1, img2):
+    if img1.shape[0] != 1: raise Exception("Image of shape [1,H,W] required.")
+    img1, img2 = img1.to(torch.float64), img2.to(torch.float64)
+    mse = img1.sub(img2).pow(2).mean()
+    if mse == 0: return float("inf")
+    else: return 20 * torch.log10(255.0/torch.sqrt(mse)).item()
 
-def structure_similarity_index(sample, original):
-    """
-    Write code to calculate SSIM. Return in float
-    """
-    pass
+def structure_similarity_index(img1, img2):
+    if img1.shape[0] != 1: raise Exception("Image of shape [1,H,W] required.")
+    # Constants
+    window_size, channels = 11, 1
+    K1, K2, DR = 0.01, 0.03, 255
+    C1, C2 = (K1*DR)**2, (K2*DR)**2
+
+    window = torch.randn(11)
+    window = window.div(window.sum())
+    window = window.unsqueeze(1).mul(window.unsqueeze(0)).unsqueeze(0).unsqueeze(0)
+    
+    mu1 = F.conv2d(img1, window, padding=window_size//2, groups=channels)
+    mu2 = F.conv2d(img2, window, padding=window_size//2, groups=channels)
+    mu12 = mu1.pow(2).mul(mu2.pow(2))
+
+    sigma1_sq = F.conv2d(img1 * img1, window, padding=window_size//2, groups=channels) - mu1.pow(2)
+    sigma2_sq = F.conv2d(img2 * img2, window, padding=window_size//2, groups=channels) - mu2.pow(2)
+    sigma12 =  F.conv2d(img1 * img2, window, padding=window_size//2, groups=channels) - mu12
+
+
+    SSIM_n = (2 * mu1 * mu2 + C1) * (2 * sigma12 + C2)
+    denom = ((mu1**2 + mu2**2 + C1) * (sigma1_sq + sigma2_sq + C2))
+    return torch.clamp((1 - SSIM_n / (denom + 1e-8)), min=0.0, max=1.0).mean().item()
